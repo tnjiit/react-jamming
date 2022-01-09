88137682ad973b0243f26566b2cf17e5865aebc0
tusharjain <tnjiitr@gmail.com>
2022-01-08 23:31:43 -0800

    Connecting the search bar
    Connecting search functionality through the components.
    At the moment on search, only term is logged on console.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -35,6 +35,8 @@ class App extends React.Component {
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
     }
 
     addTrack(track){
@@ -54,15 +56,36 @@ class App extends React.Component {
         this.setState({playlistName: name});
     }
 
+    savePlaylist(){
+        let trackURIs = this.state.playlistTracks.map(
+            track => {
+                return track.uri;
+            }
+        );
+    }
+
+    search(term){
+        console.log(term);
+    }
+
     render(){
         return (
                 <div>
                     <h1>Ja<span className="highlight">mmm</span>ing</h1>
                     <div className="App">
-                        <SearchBar />
+                        <SearchBar
+                            onSearch={this.search}
+                        />
                         <div className="App-playlist">
-                            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-                            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+                            <SearchResults searchResults={this.state.searchResults}
+                                onAdd={this.addTrack}
+                            />
+                            <Playlist playlistName={this.state.playlistName}
+                                playlistTracks={this.state.playlistTracks}
+                                onRemove={this.removeTrack}
+                                onNameChange={this.updatePlaylistName}
+                                onSave={this.savePlaylist}
+                            />
                         </div>
                     </div>
                 </div>
